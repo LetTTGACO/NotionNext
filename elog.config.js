2622c09c452543dcc24e3b976f0e804398d5e98d
LetTTGACO <1225751694@qq.com>
2023-09-03 12:10:39 +0800

    feat: elog

---
+++ elog.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  write: {
+    platform: 'notion',
+    notion: {
+      token: process.env.NOTION_TOKEN,
+      databaseId: process.env.NOTION_DATABASE_ID,
+      filter: {
+        and: [
+          {
+            property: 'type',
+            select: {
+              equals: 'Post'
+            }
+          },
+          {
+            property: 'status',
+            select: {
+              equals: 'Published'
+            }
+          }
+        ]
+      },
+      sorts: 'updateTimeDesc'
+    }
+  },
+  deploy: {
+    platform: 'local',
+    local: {
+      outputDir: './docs',
+      filename: 'title',
+      format: 'markdown',
+      catalog: false,
+      formatExt: ''
+    }
+  },
+  image: {
+    enable: true,
+    platform: 'cos',
+    cos: {
+      secretId: process.env.COS_SECRET_ID,
+      secretKey: process.env.COS_SECRET_KEY,
+      bucket: process.env.COS_BUCKET,
+      region: process.env.COS_REGION,
+      host: process.env.COS_HOST,
+      prefixKey: '1874-blog-images',
+      secretExt: '' // 可选
+    }
+  }
+}
